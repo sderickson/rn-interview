3f30822bae32a307f55cb3d38f218a896f2bbbbb
Scott Erickson <sderickson@gmail.com>
2025-10-07 10:26:40 -0700

    Add some items

--- app/App.tsx
+++ app/App.tsx
@@ -22,13 +22,55 @@ function App() {
   );
 }
 
+
+const items = [
+  {
+    id: 1,
+    title: 'Item 1',
+  },
+  {
+    id: 2,
+    title: 'Item 2',
+  },
+  {
+    id: 3,
+    title: 'Item 3',
+  },
+  {
+    id: 4,
+    title: 'Item 4',
+  },
+  {
+    id: 5,
+    title: 'Item 5',
+  },
+  {
+    id: 6,
+    title: 'Item 6',
+  },
+  {
+    id: 7,
+    title: 'Item 7',
+  },
+  {
+    id: 8,
+    title: 'Item 8',
+  },
+  {
+    id: 9,
+    title: 'Item 9',
+  },
+]
+
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
 
   return (
     <View style={styles.container}>
       <View style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flex: 1, paddingTop: safeAreaInsets.top, paddingBottom: safeAreaInsets.bottom, paddingLeft: 8, paddingRight: 8 }}>
-        <Text>Build your solution here?</Text>
+        {items.map((item) => (
+          <Text key={item.id}>{item.title}</Text>
+        ))}
       </View>
     </View>
   );
